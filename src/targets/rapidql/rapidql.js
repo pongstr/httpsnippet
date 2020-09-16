5bd79e8d3fe5c0dccedbe2117f21e9c550811c33
Omri Peleg <omrip@rapidapi.com>
2020-09-16 19:46:45 +0300

    Fix Typo

--- src/targets/rapidql/rapidql.js
+++ src/targets/rapidql/rapidql.js
@@ -14,14 +14,13 @@ module.exports = function (source, options) {
   code.push('// For more information about RapidQL, checkout docs.rapidql.com!');
   code.push('');
   code.blank();
-  code.push('// For more information about RapidQL, checkout docs.rapidql.com!');
   code.push("const RapidQL = require('RapidQL');");
   code.push('let rql = new RapidQL({');
   code.push('});');
   code.push('');
   code.push('rql.query(`{');
   code.push(`  Http.${(source.method || '').toLowerCase()}(`);
-  code.push(`    url:"${helpers.quote(source.fullUrl)}"`);
+  code.push(`    url:"${source.fullUrl}"`);
 
   if (source.headers.length) {
     code.push(`    headers : {\n${Object.entries(source.allHeaders).map(([key, val]) => `"${key}":"${val}"`).join(",\n")}\n    }`);
