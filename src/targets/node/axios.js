43b0c18a113a8e5ebdfaebf211d1d2e273ac00d5
Sahar Bechor <saharbechor@rapidapi.com>
2022-03-22 17:11:00 +0200

    node-axios update application/form-data and x-www-form-urlencoded

--- src/targets/node/axios.js
+++ src/targets/node/axios.js
@@ -20,8 +20,11 @@ module.exports = function (source, options) {
 
   const code = new CodeBuilder(opts.indent)
 
-  code.push('var axios = require("axios").default;')
-    .blank()
+  code.push('var axios = require("axios");')
+
+  if (source.postData.params && source.postData.params.some(function (param) { return param.fileName; })) {
+    code.push('var fs = require("fs");')
+  }
 
   const reqOpts = {
     method: source.method,
@@ -37,24 +40,75 @@ module.exports = function (source, options) {
   }
 
   switch (source.postData.mimeType) {
-    case 'application/x-www-form-urlencoded':
-      reqOpts.data = source.postData.paramsObj
-      break
-
     case 'application/json':
+      code.blank();
       if (source.postData.jsonObj) {
         reqOpts.data = source.postData.jsonObj
       }
       break
 
+    case 'application/x-www-form-urlencoded':
+      code.push('var qs = require("qs");')
+        .blank()
+        .push('var data = qs.stringify({')
+
+      source.postData.params.forEach(function (param, index, arr) {
+        const value = param.value !== undefined ? param.value : ''
+        const isLast = index === arr.length - 1
+        
+        code.push(
+          '%s%s: %s%s',
+          opts.indent,
+          JSON.stringify(param.name),
+          JSON.stringify(value),
+          isLast ? '' : ','
+        )
+      })
+
+      code.push('});')
+        .blank()
+
+      reqOpts.data = '[data]'
+      break
+      
+    case 'multipart/form-data':
+      delete reqOpts.headers['Content-Type']
+      delete reqOpts.headers['content-type'] // content-type header will come from the data.getHeaders() with the right boundary
+      reqOpts.headers.placeholderapid = 'placeholderapid'
+      code.push('var FormData = require("form-data");')
+        .blank()
+        .push('var data = new FormData();')
+
+      source.postData.params.forEach(function (param) {
+        if (param.fileName) {
+          code.push('data.append(%s, fs.createReadStream("/PATH/TO/%s"));', JSON.stringify(param.name), param.fileName)
+        } else {
+          const value = param.value !== undefined ? param.value.toString() : ''
+
+          code.push(
+            'data.append(%s, %s);',
+            JSON.stringify(param.name),
+            JSON.stringify(value)
+          )
+        }
+      })
+
+      code.blank()
+
+      reqOpts.data = '[data]'
+      break
+
     default:
+      code.blank()
       if (source.postData.text) {
         reqOpts.data = source.postData.text
       }
   }
 
-  code.push('var options = %s;', stringifyObject(reqOpts, { indent: '  ', inlineCharacterLimit: 80 }))
+  code.push('var options = %s;', stringifyObject(reqOpts, { indent: '  ', inlineCharacterLimit: 80 })
+    .replace("'[data]'", 'data').replace("placeholderapid: 'placeholderapid'", '...data.getHeaders()'))
     .blank()
+
 
   code.push(util.format('axios.request(options).then(%s', 'function (response) {'))
     .push(1, 'console.log(response.data);')
